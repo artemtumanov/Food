822e96e70185914272657d90a4e4e90da46b7e0e
Artem Tumanov <artem.tumanov.93@inbox.ru>
2024-02-26 10:38:13 +0300

    add timer and modal functional

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,8 +3,10 @@ var __webpack_exports__ = {};
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
+// 'use strict'
 document.addEventListener('DOMContentLoaded', () => {
   // Реализуем функционал табов на странице
+
   const tabContent = document.querySelectorAll('.tabcontent'),
     // Контент наших табов
     tabItems = document.querySelector('.tabheader__items'),
@@ -44,6 +46,90 @@ document.addEventListener('DOMContentLoaded', () => {
           showActiveTab(i);
         }
       });
+    }
+  });
+
+  //Реализуем функционал таймера на странице
+
+  const deadline = '2024-02-22T09:09:51.294Z';
+  function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      hours = Math.floor(t / (1000 * 60 * 60) % 24),
+      minutes = Math.floor(t / (1000 * 60) % 60),
+      seconds = Math.floor(t / 1000 % 60);
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+  function addZero(time) {
+    if (time >= 0 && time < 10) {
+      return `0${time}`;
+    } else {
+      return time;
+    }
+  }
+  function setClock(endtime, selector) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+      days.innerHTML = addZero(t.days);
+      hours.innerHTML = addZero(t.hours);
+      minutes.innerHTML = addZero(t.minutes);
+      seconds.innerHTML = addZero(t.seconds);
+      if (t.total <= 1) {
+        clearInterval(timeInterval);
+        days.innerHTML = '00';
+        hours.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';
+      }
+    }
+  }
+  setClock(deadline, '.timer');
+
+  // Реализуем функционал модального окна
+
+  //Получаем наши кнопки по дата атрибутам и наше модальное окно.
+  const modalBtn = document.querySelectorAll('[data-modal]'),
+    closeBtn = document.querySelector('[data-close]'),
+    modal = document.querySelector('.modal');
+
+  // Навесим на все наши кнопки открытие модалки
+  modalBtn.forEach(item => {
+    item.addEventListener('click', () => {
+      modal.style.display = 'block'; // Показываем модалку
+      document.body.style.overflow = 'hidden'; // Отменяем прокрутку страницы при открытой модалке
+    });
+  });
+
+  // Напишем функцию закрытия модалки
+  function closeModal() {
+    modal.style.display = 'none'; // Закрываем модалку
+    document.body.style.overflow = 'visible'; // Возвращаем прокрутку
+  }
+
+  // Навесим на кнопку крестика закрытие модалки
+  closeBtn.addEventListener('click', closeModal);
+
+  // Сделаем закрытие модалки при клике на оверлей 
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  // Закрытие модалки на клавишу ESC
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      closeModal();
     }
   });
 });
