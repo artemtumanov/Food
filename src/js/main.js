02f2e0e16977cc229fe9a446cc7a5560f910d988
Artem Tumanov <artem.tumanov.93@inbox.ru>
2024-02-27 11:03:51 +0300

    add classes and rest

--- src/js/main.js
+++ src/js/main.js
@@ -139,9 +139,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	})
 
 	// Реализуем функционал появления модалки через какое-то время (например, 5 сек)
-	const openModalByTimer = setTimeout(openModal, 5000);
+	// const openModalByTimer = setTimeout(openModal, 5000);
 	
 	// Реализуем функционал открытия модалки после прокуртки страницы до конца
+	// Напишем функцию, которая сравнивает количество отскроленного + высоту показываемого контента со всей высотой документа и открывает модалку. После чего удаляет обработчик события, чтобы не повторялся.
 	function showModalByScroll() {
 		if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
 			openModal();
@@ -149,5 +150,77 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	window.addEventListener('scroll', showModalByScroll);
-})
+	// window.addEventListener('scroll', showModalByScroll);
+
+
+	// Используем классы
+
+	class MenuCard {
+		constructor (src, alt, title, descr, price, parentSelector, ...classes) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.descr = descr;
+			this.price = price;
+			this.classes = classes;
+			this.transfer = 9;
+			this.parent = document.querySelector(parentSelector);
+			this.changeToUAH();
+		}
+		changeToUAH() {
+			this.price = this.price * this.transfer;
+		}
+		render() {
+			const element = document.createElement('div');
+			
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else {
+				this.classes.forEach(classItem => element.classList.add(classItem));
+			}
+
+			element.innerHTML = `
+				<img src=${this.src} alt=${this.alt}>
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Цена:</div>
+					<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	new MenuCard(
+		"img/tabs/vegy.jpg",
+		"vegy",
+		'Меню "Фитнес"',
+		'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+		26,
+		'.menu__field .container',
+	).render();
+
+	new MenuCard(
+		"img/tabs/elite.jpg",
+		"elite",
+		'Меню “Премиум”',
+		'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+		46,
+		'.menu__field .container',
+		'menu__item',
+	).render();
+
+	new MenuCard(
+		"img/tabs/post.jpg",
+		"post",
+		'Меню "Постное"',
+		'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+		30,
+		'.menu__field .container',
+		'menu__item'
+	).render();
+});
+
